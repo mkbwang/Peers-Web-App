5c83d0c0c1a9f1160a77184eb0f881e5ccfac33c
wangmk <wangmk@umich.edu>
2018-11-03 18:58:49 -0400

    Deleting trial files. Javascript for plotting part later.

--- peers/js/main.jsx
+++ peers/js/main.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Prob from './prob';
 
-ReactDOM.render(
-    <Kmplot url="/api/kmplot/"/>,
-    document.getElementById('kmplot'),
-);
-
 
 ReactDOM.render(
     <Prob url="/api/prob/" />,
