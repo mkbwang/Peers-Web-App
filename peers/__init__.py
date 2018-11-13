803db73da21abdbfc264ca45cbcfe259ec479873
Mukai Wang <wangmk@umich.edu>
2018-11-13 18:50:06 -0500

    Update __init__.py
    Change the comments.

--- peers/__init__.py
+++ peers/__init__.py
@@ -3,7 +3,7 @@ import flask
 # app is a single object used by all the code modules in this package
 app = flask.Flask(__name__)  # pylint: disable=invalid-name
 
-# Read settings from config module (insta485/config.py)
+# Read settings from config module (peers/config.py)
 app.config.from_object('peers.config')
 
 # Overlay settings read from file specified by environment variable. This is
