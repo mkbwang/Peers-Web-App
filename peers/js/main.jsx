65a07e2842456d22e3076fc80817e9472213a682
wangmk <wangmk@umich.edu>
2019-02-01 13:20:39 -0500

    Delete unnecessary files

--- peers/js/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Prob from './prob';
-
-
-ReactDOM.render(
-    <Prob url="/api/prob/" />,
-    document.getElementById('probability'),
-);
